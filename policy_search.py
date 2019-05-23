f369722e29a357fd9249148c510aafde65cdf76e
sanky29 <sangandhi29@gmail.com>
2019-05-23 17:32:21 +0530

    secomd update

--- policy_search.py
+++ policy_search.py
@@ -2,33 +2,31 @@ import random
 
 class ps:
     def __init__(self,k,a,b):
-        self.value= [0.0]*k
-        self.nt = [0.0] * k
         self.a = a
         self.b = b
-        self.track = [0.0]*k
+        self.pro = [0.0]*k
         for i in range(0,k ):
-            self.track[i] = 1/k
+            self.pro[i] = 1/k
 
     def select_arm(self):
         l = random.uniform(0,1)
         i = 0
         j = 0.0
         k = True
-        while(k and i < len(self.track)):
-            if (l < self.track[i] + j):
+        while(k and i < len(self.pro)):
+            if (l < self.pro[i] + j):
                 k = False
             else:
-                j = j + self.track[i]
+                j = j + self.pro[i]
                 i += 1
-        return(min(i,len(self.track) - 1))
+        return(min(i,len(self.pro) - 1))
     def update(self,i,reward):
-        z = self.track[i]
+        z = self.pro[i]
         if (reward == 1):
-            for j in range(0,len(self.track)):
-                 self.track[j] = self.track[j]*(1 - self.a)
-            self.track[i] = z + self.a * (1 - z)
+            for j in range(0,len(self.pro)):
+                 self.pro[j] = self.pro[j]*(1 - self.a)
+            self.pro[i] = z + self.a * (1 - z)
         else:
-            for j in range(0, len(self.track)):
-                self.track[j] = (self.track[j] + self.b*(1 - self.track[j]))
-            self.track[i] = z * (1 - self.b)
+            for j in range(0, len(self.pro)):
+                self.pro[j] = (self.pro[j] + self.b*(1 - self.pro[j]))
+            self.pro[i] = z * (1 - self.b)
